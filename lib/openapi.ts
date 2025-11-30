77c271f1daecb1088922a57fc7ed03ad3518068e
noreplay <jekji125@protonmail.com>
2025-11-30 16:17:35 +0800

    openAPIFetch

--- lib/openapi.ts
+++ lib/openapi.ts
@@ -1,6 +1,17 @@
 import { createOpenAPI } from 'fumadocs-openapi/server';
 
+// export const openapi = createOpenAPI({
+//   // input files
+//   input: ['./openapi.json'],
+// });
+
 export const openapi = createOpenAPI({
-  // input files
-  input: ['./openapi.json'],
+  async input() {
+    return {
+      // [id]: downloaded OpenAPI Schema
+      my_schema: await fetch(
+        'https://registry.scalar.com/@scalar/apis/galaxy/latest?format=json',
+      ).then((res) => res.json()),
+    };
+  },
 });
